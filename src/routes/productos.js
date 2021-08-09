0bb7a9008d66f31b8d5f3c6d7643cc4b81cd14ea
Adrian Zambrano <jadrianzc99@gmail.com>
2021-08-09 00:06:09 -0500

    Pull and merge rama yorly

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,51 +1,51 @@
-const express = require("express");
+const express = require('express');
 const productos = express.Router();
-const modelo = require("../models/ModeloProductos"); //obtener modelo
+const modelo = require('../models/ModeloProductos'); //obtener modelo
 
 /*EJEMPLO*/
 
 // Recibir datos
-productos.get("/", async (req, res) => {
-  const documentos = await modelo.find();
-  //console.log(documentos);
-  res.json(documentos);
+productos.get('/', async (req, res) => {
+	const documentos = await modelo.find();
+	//console.log(documentos);
+	res.json(documentos);
 });
 
 // Recibir datos por id
 
-productos.get("/:id", async (req, res) => {
-  const documentos = await modelo.findById(req.params.id);
-  //console.log(documentos);
-  res.json(documentos);
+productos.get('/:id', async (req, res) => {
+	const documentos = await modelo.findById(req.params.id);
+	console.log(documentos);
+	res.json(documentos);
 });
 
 // Enviar datos
-productos.post("/", async (req, res) => {
-  const f_registro_producto = new Date().toLocaleString("es-EC");
-  const {
-    imgurl,
-    codigo_producto,
-    nom_producto,
-    descrip_producto,
-    categoria_producto,
-    precio_producto,
-    marca_producto,
-    cantidad_producto,
-  } = req.body;
-  const documento = new modelo({
-    imgurl,
-    codigo_producto,
-    nom_producto,
-    descrip_producto,
-    categoria_producto,
-    precio_producto,
-    marca_producto,
-    cantidad_producto,
-    f_registro_producto,
-  });
-  await documento.save();
+productos.post('/', async (req, res) => {
+	const f_registro_producto = new Date().toLocaleString('es-EC');
+	const {
+		imgurl,
+		codigo_producto,
+		nom_producto,
+		descrip_producto,
+		categoria_producto,
+		precio_producto,
+		marca_producto,
+		cantidad_producto,
+	} = req.body;
+	const documento = new modelo({
+		imgurl,
+		codigo_producto,
+		nom_producto,
+		descrip_producto,
+		categoria_producto,
+		precio_producto,
+		marca_producto,
+		cantidad_producto,
+		f_registro_producto,
+	});
+	await documento.save();
 
-  res.json({ status: "Guardado" });
+	res.json({ status: 'Guardado' });
 });
 /*
 // Actualizar datos
